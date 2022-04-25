e7d6b29987a0f5683d5914dfc6b91876e82972a5
Deyvid Freitas <shapperz@hotmail.com>
2022-04-25 10:34:40 -0300

    first commit

--- tesla-clone/src/App.js
+++ tesla-clone/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
@@ -8,7 +7,7 @@ import Home from './components/Home'
 function App() {
   return (
     <div>
-      {/* <Header />*/}
+      <Header />
       <Home />
       
     </div>
--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
 function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
+
+
   return (
     <Wrap bgImage={backgroundImg}>
-
+      <Fade bottom>
         <ItemText>
             <h1>{title}</h1>
             <p>{description}</p>
         </ItemText>
+      </Fade>
       <Buttons>
+        <Fade bottom>
         <ButtonGroup>
           <LeftButton>
            {leftBtnText}
@@ -21,6 +26,7 @@ function Section({title, description, leftBtnText, rightBtnText, backgroundImg})
           }
          
         </ButtonGroup>
+       </Fade> 
         <DownArrow src='/image/down-arrow.svg'/>
       </Buttons>
     </Wrap>
@@ -30,6 +36,7 @@ function Section({title, description, leftBtnText, rightBtnText, backgroundImg})
 export default Section
 
 const Wrap = styled.div`
+
     width: 100vw;
     height:100vh;
     background-side:cover;
@@ -46,6 +53,7 @@ const Wrap = styled.div`
 const ItemText = styled.div`
     padding-top:15vh;
     text-aling:center;
+    z-index: -1;
     
 `
 const ButtonGroup = styled.div`
